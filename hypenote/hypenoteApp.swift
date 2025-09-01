d9f6b3a8185f948f20b1d10631c093de5bb5e3fd
Ari Victor <11803028+arivictor@users.noreply.github.com>
2025-09-01 21:12:59 +1000

    Initial Commit

---
+++ hypenote/hypenoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  hypenoteApp.swift
+//  hypenote
+//
+//  Created by Ari Laverty on 1/9/2025.
+//
+
+import SwiftUI
+
+@main
+struct hypenoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ hypenoteTests/hypenoteTests.swift
@@ -0,0 +1,17 @@
+//
+//  hypenoteTests.swift
+//  hypenoteTests
+//
+//  Created by Ari Laverty on 1/9/2025.
+//
+
+import Testing
+@testable import hypenote
+
+struct hypenoteTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
